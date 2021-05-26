d3c3a8b56fcfcdbab8d972b74a6a7cb5d8405404
jsh <tmdgusya@naver.com>
2021-05-27 00:29:54 +0900

    .env env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,19 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.test.env',
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     TypeOrmModule.forRoot({
+      name: 'dev',
       type: 'postgres',
       host: 'localhost',
       port: 5432,
