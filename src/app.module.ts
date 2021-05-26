7cfcf52fd74f7f8828bb320e1ec4808efdb3dfad
jsh <tmdgusya@naver.com>
2021-05-27 00:15:16 +0900

    dependency: TypeORM Configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,25 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'jeongseunghyeon',
+      password: '12345',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+      entities: ['src/entity/**/*.ts'],
+      migrations: ['src/migration/**/*.ts'],
+      subscribers: ['src/subcribe/**/*.ts'],
     }),
     RestaurantsModule,
   ],
