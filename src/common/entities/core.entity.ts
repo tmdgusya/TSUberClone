ea20b12d5884926a9a4d35e5722eb1066f1f05ef
jsh <tmdgusya@naver.com>
2021-05-29 13:41:31 +0900

    feat: CoreEntity to Graphql Model

--- src/common/entities/core.entity.ts
+++ src/common/entities/core.entity.ts
@@ -4,15 +4,19 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Field } from '@nestjs/graphql';
 
 @Entity()
 export class CoreEntity {
   @PrimaryGeneratedColumn()
+  @Field(type => Number)
   id: number;
 
   @CreateDateColumn()
+  @Field(type => Date)
   createdAt: Date;
 
   @UpdateDateColumn()
+  @Field(type => Date)
   updatedAt: Date;
 }
