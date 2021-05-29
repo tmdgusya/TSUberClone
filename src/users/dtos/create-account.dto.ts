6ae1caad2d3c689d0cd2467b958e4c688da4fcee
jsh <tmdgusya@naver.com>
2021-05-29 13:42:04 +0900

    feat: create UserDTO

---
+++ src/users/dtos/create-account.dto.ts
@@ -0,0 +1,18 @@
+import { PickType, Field, InputType, ObjectType } from '@nestjs/graphql';
+import { User } from '../entities/user.entity';
+
+@InputType()
+export class CreateAccountInput extends PickType(User, [
+  'email',
+  'password',
+  'role',
+]) {}
+
+@ObjectType()
+export class CreateAccountOutput {
+  @Field(type => String, { nullable: true })
+  error?: string;
+
+  @Field(type => Boolean)
+  ok: boolean;
+}
