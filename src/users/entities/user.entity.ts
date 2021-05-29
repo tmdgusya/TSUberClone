d51e8cb2a6a7546e8d2cfa0fc578baa93f433c0a
jsh <tmdgusya@naver.com>
2021-05-29 13:42:14 +0900

    feat: UserEntity 생성

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,16 +1,37 @@
 import { Entity, Column } from 'typeorm';
 import { CoreEntity } from 'src/common/entities/core.entity';
+import {
+  ObjectType,
+  InputType,
+  Field,
+  registerEnumType,
+} from '@nestjs/graphql';
 
+enum UserRole {
+  OWNER,
+  CLIENT,
+  DELEVERY,
+}
+
+registerEnumType(UserRole, { name: 'UserRole' });
+
+@InputType({ isAbstract: true })
 @Entity()
+@ObjectType()
 export class User extends CoreEntity {
   @Column()
+  @Field(type => String)
   email: string;
 
   @Column()
+  @Field(type => String)
   password: string;
 
-  @Column()
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.CLIENT,
+  })
+  @Field(type => UserRole, { defaultValue: UserRole.CLIENT })
   role: UserRole;
 }
-
-type UserRole = 'CLIENT' | 'OWNER' | 'DELIVERY';
