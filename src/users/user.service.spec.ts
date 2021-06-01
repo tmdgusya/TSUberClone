a9201be882ce7cd4a6c14e6ac50c2ba1e0faee00
jsh <tmdgusya@naver.com>
2021-06-01 22:27:28 +0900

    test: 회원가입 테스트 작성

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -6,12 +6,13 @@ import { Verfication } from './entities/verification.entity';
 import { JwtService } from 'src/jwt/jwt.service';
 import { MailService } from 'src/mail/mail.service';
 import { Repository } from 'typeorm';
+import { ErrorMessage } from 'src/error/error_message';
 
-const mockRepository = {
+const mockRepository = () => ({
   findOne: jest.fn(),
   save: jest.fn(),
   create: jest.fn(),
-};
+});
 
 const jwtMockService = {
   sign: jest.fn(),
@@ -19,27 +20,33 @@ const jwtMockService = {
 };
 
 const mailMockService = {
-  sensendVerificationEmail: jest.fn(),
+  sendVerificationEmail: jest.fn(),
 };
 
-type MockRepository<T = any> = Partial<
+type UserMockRepository<T = any> = Partial<
   Record<keyof Repository<User>, jest.Mock>
+>;
+
+type VerificationMockRepository<T = any> = Partial<
+  Record<keyof Repository<Verfication>, jest.Mock>
 >;
 
 describe('User Service Test', () => {
   let service: UserService;
-  let userRepository: MockRepository;
+  let userRepository: UserMockRepository;
+  let verificationRepository: VerificationMockRepository;
+  let emailService: MailService;
   beforeAll(async () => {
     const module = await Test.createTestingModule({
       providers: [
         UserService,
         {
           provide: getRepositoryToken(User),
-          useValue: mockRepository,
+          useValue: mockRepository(),
         },
         {
           provide: getRepositoryToken(Verfication),
-          useValue: mockRepository,
+          useValue: mockRepository(),
         },
         {
           provide: JwtService,
@@ -53,6 +60,8 @@ describe('User Service Test', () => {
     }).compile();
     service = module.get<UserService>(UserService);
     userRepository = module.get(getRepositoryToken(User));
+    verificationRepository = module.get(getRepositoryToken(Verfication));
+    emailService = module.get<MailService>(MailService);
   });
 
   it('bed defined', () => {
@@ -60,7 +69,57 @@ describe('User Service Test', () => {
   });
 
   describe('createAccount', () => {
-    it('should fail if user exist', () => {});
+    const defaultCreateArgs = {
+      email: 'test@test.com',
+      password: 'Good',
+      role: 0,
+    };
+
+    it('should fail if user exist', async () => {
+      userRepository.findOne.mockResolvedValue({
+        id: 1,
+        email: 'test@test.com',
+        password: 'test',
+      });
+      expect(await service.createAccount(defaultCreateArgs)).toEqual({
+        ok: false,
+        error: ErrorMessage.USER_EXIST_ERROR,
+      });
+    });
+
+    it('should create a new user', async () => {
+      userRepository.findOne.mockReturnValue(undefined);
+      userRepository.create.mockReturnValue(defaultCreateArgs);
+      userRepository.save.mockResolvedValue({ user: defaultCreateArgs });
+      verificationRepository.create.mockReturnValue(defaultCreateArgs);
+      verificationRepository.save.mockResolvedValue({
+        code: 'code',
+      });
+
+      const result = await service.createAccount(defaultCreateArgs);
+
+      expect(userRepository.create).toHaveBeenCalledTimes(1);
+      expect(userRepository.create).toHaveBeenCalledWith(defaultCreateArgs);
+      expect(userRepository.save).toHaveBeenCalledTimes(1);
+      expect(userRepository.save).toHaveBeenCalledWith(defaultCreateArgs);
+      expect(verificationRepository.create).toHaveBeenCalledTimes(1);
+      expect(verificationRepository.create).toHaveBeenCalledWith({
+        user: defaultCreateArgs,
+      });
+      expect(verificationRepository.save).toHaveBeenCalledTimes(1);
+      expect(verificationRepository.save).toHaveBeenCalledWith(
+        defaultCreateArgs,
+      );
+      expect(emailService.sendVerificationEmail).toHaveBeenCalledTimes(1);
+      expect(emailService.sendVerificationEmail).toHaveBeenCalledWith(
+        expect.any(String),
+        expect.any(String),
+        expect.any(String),
+      );
+      expect(result).toEqual({
+        ok: true,
+      });
+    });
   });
   it.todo('login');
   it.todo('findById');
