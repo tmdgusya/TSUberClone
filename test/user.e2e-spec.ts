6d34bf79ed42eabab90c4947d194d5da1e63a0ee
jsh <tmdgusya@naver.com>
2021-06-06 22:24:34 +0900

    test: create account E2E test

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -2,6 +2,16 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
+import * as request from 'supertest';
+import { ErrorMessage } from 'src/error/error_message';
+
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -20,9 +30,61 @@ describe('UserModule (e2e)', () => {
     await app.close();
   });
 
+  describe('createAccount', () => {
+    const userInfo = {
+      email: 'tmdgusya@test.com',
+      password: 'test12',
+      role: 'OWNER',
+    };
+
+    it('should create Account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `mutation {
+            createAccount(input : {
+              email : "${userInfo.email}",
+              password: "${userInfo.password}",
+              role: ${userInfo.role}
+            }) {
+              ok
+              error
+            }
+          }`,
+        })
+        .expect(200)
+        .expect(res => {
+          expect(res.body.data.createAccount.ok).toBeTruthy();
+          expect(res.body.data.createAccount.error).toBeNull();
+        });
+    });
+
+    it('shoud fail if account already exist', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `mutation {
+            createAccount(input : {
+              email : "${userInfo.email}",
+              password: "${userInfo.password}",
+              role: ${userInfo.role}
+            }) {
+              ok
+              error
+            }
+          }`,
+        })
+        .expect(200)
+        .expect(res => {
+          expect(res.body.data.createAccount.ok).toBeFalsy();
+          expect(res.body.data.createAccount.error).toEqual(
+            ErrorMessage.USER_EXIST_ERROR,
+          );
+        });
+    });
+  });
   it.todo('me');
   it.todo('userProfile');
-  it.todo('createAccount');
   it.todo('login');
   it.todo('editProfile');
   it.todo('verifyEmail');
